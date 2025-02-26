89ece8cd18f163d0b0c46b8d30a382b7c278ebfd
unknown <luiarroyo@gmail.com>
2025-02-25 19:38:37 -0600

    final

--- Weatherpy/Starter_Code/api_keys.py
+++ Weatherpy/Starter_Code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "5036810bf8c18e045051b2ab3adb4886"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "058ef75aaf47487e8b38c7b37dc0e997"
